ca50f1ac1f8ee9ef1175dc172898ffc93013f54f
koji-webfolio-2025 <codeshift2025@gmail.com>
2025-06-10 14:52:37 +0900

    fix: Fortify認証ルートをwebグループに

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -4,11 +4,11 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use Laravel\Fortify\Http\Controllers\AuthenticatedSessionController;
 
-Route::post('/login', [AuthenticatedSessionController::class, 'store'])->name('login');
-//Route::post('/login', [AuthenticatedSessionController::class, 'store'])->middleware('guest');
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->middleware('auth');
+Route::middleware(['web', 'guest'])->post('/login', [AuthenticatedSessionController::class, 'store']);
+Route::middleware(['web', 'auth'])->post('/logout', [AuthenticatedSessionController::class, 'destroy']);
 
-Route::middleware('auth')->get('/api/user', function (Request $request) {
+// ↓API用ユーザー情報
+Route::middleware(['web', 'auth'])->get('/api/user', function (Request $request) {
     return response()->json([
         'cookie'  => $request->cookies->all(),
         'session' => session()->all(),
