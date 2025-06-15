f880a3191036a3eab6b95db0c50bb4e29a82e921
koji-webfolio-2025 <codeshift2025@gmail.com>
2025-06-15 15:51:20 +0900

    h2文言変更テスト

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -10,7 +10,7 @@
           <label for="password">パスワード</label>
           <input id="password" v-model="password" type="password" required />
         </div>
-        <button type="submit">ログインテスト</button>
+        <button type="submit">ログイン</button>
         <div v-if="error" class="error">{{ error }}</div>
       </form>
     </div>
