414ddfb47e093acafc9c0093023f7a149cc7dd86
koji-webfolio-2025 <codeshift2025@gmail.com>
2025-06-16 20:55:24 +0900

    Fix deployment workflow and update LoginView

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -10,7 +10,7 @@
           <label for="password">パスワード</label>
           <input id="password" v-model="password" type="password" required />
         </div>
-        <button type="submit">ログインテスト１</button>
+        <button type="submit">ログインテスト2</button>
         <div v-if="error" class="error">{{ error }}</div>
       </form>
     </div>
